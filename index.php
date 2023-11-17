1eaf8d8e0c0a3785f5953f243da33c54f08c2886
Dubois322 <129506193+Dubois322@users.noreply.github.com>
2023-11-18 00:04:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,22 +11,22 @@
 <h3>Author Details</h3>
 <form action="process-form.php" method="POST">
      <!-- AuthorId (PK) - Assuming it's auto-incremented in the database -->
-    <label for="authorFullname">Fullname:</label><br>
-    <input type="text" name="authorFullname" id="authorFullname" placeholder="Enter the author-Fullname" maxlength="60" required /><br><br>
+    <label for="Author Fullname">Author'sFullname:</label><br>
+    <input type="text" name="authorFullname" id="authorFullname" placeholder="Enter the author-Fullname" maxlength="50" required /><br><br>
 
-    <label for="authorEmail"> Email:</label><br>
-    <input type="email" name="authorEmail" id="authoremail" placeholder="Enter the author-Email" maxlength="60" /><br><br>
+    <label for="Author Email"> Email:</label><br>
+    <input type="email" name="authorEmail" id="authoremail" placeholder="Enter the author-Email" maxlength="50" /><br><br>
 
-    <label for="authoraddress">Address:</label><br>
-    <input type="text" name="authoraddress" id="authoraddress" placeholder="Enter the author-address" maxlength="160" required /><br><br>
+    <label for="Author address">Address:</label><br>
+    <input type="text" name="authoraddress" id="authoraddress" placeholder="Enter the author-address" maxlength="100" required /><br><br>
 
-    <label for="authorBiography">Biography:</label><br>
+    <label for="Author Biography">Biography:</label><br>
     <textarea name="authorBiography" id="authorBiography" placeholder="Enter the Biography" rows="4" required></textarea><br><br>
 
-    <label for="authorDateOfBirth">Date of Birth:</label>
+    <label for="AuthorDateOfBirth">Date of Birth:</label>
     <input type="date" id="authorDateOfBirth" name="authorDateOfBirth" required><br>
 
-    <label for="authorSuspended">Suspended:</label>
+    <label for="AuthorSuspended">Suspended:</label>
     <input type="checkbox" id="authorSuspended" name="authorSuspended"><br>
 
 
